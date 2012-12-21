736b61d70eed78c7505b9b7accdeaf55f80674f6
Dayton Nolan <dnolan@gmail.com>
2012-12-21 02:19:42 -0600

    Remove gemset and related functions

--- gemsets.sh
+++ gemsets.sh
@@ -15,7 +15,7 @@ function switch_gemset() {
   break
 }
 
-function chruby_gemset() {
+function auto_gemsets() {
   local dir="$PWD"
   local version_file
 
@@ -37,62 +37,18 @@ function chruby_gemset() {
   done
 }
 
-function gemplode() {
-
-  if [ ! -z "$1" ]; then
-    echo "${GEMSET_ROOT}/${1}"
-    if [ -d "${GEMSET_ROOT}/${1}" ]; then
-      echo "Are you sure you want to delete the ${1} gemset? y/n"
-      read response
-
-      if [ "$response" == "y" ]; then
-        rm -Rf ${GEMSET_ROOT}/${1}
-        chruby_reset
-      else
-        echo "No gemset removed"
-      fi
-    else
-      echo "No gemset named ${1}"
-    fi
-
-  else
-    echo "Usage:"
-    echo "gemplode (gemset)"
-  fi
-}
-
-function gemset() {
-  if [ -z "$1" ]; then
-    echo "${GEMSET}"
-  else
-    case "$1" in
-      "destroy" )
-        if [ ! -z "$2" ]; then
-          gemplode $2
-        else
-          echo "Usage:"
-          echo "gemset destroy (gemset)"
-        fi
-      ;;
-      "list" )
-        ls -l "${GEMSET_ROOT}"
-      ;;
-    esac
-  fi
-}
-
 if [ ! -n "$GEMSET_ROOT" ]; then
   export GEMSET_ROOT="${HOME}/.gemsets"
 fi
 
 if [[ -n "$ZSH_VERSION" ]]; then
-  precmd_functions+=("chruby_gemset")
+  precmd_functions+=("auto_gemsets")
 else
   if [[ -n "$PROMPT_COMMAND" ]]; then
-    if [[ ! "$PROMPT_COMMAND" == *chruby_gemset* ]]; then
-      PROMPT_COMMAND="$PROMPT_COMMAND; chruby_gemset"
+    if [[ ! "$PROMPT_COMMAND" == *auto_gemsets* ]]; then
+      PROMPT_COMMAND="$PROMPT_COMMAND; auto_gemsets"
     fi
   else
-    PROMPT_COMMAND="chruby_gemset"
+    PROMPT_COMMAND="auto_gemsets"
   fi
 fi
