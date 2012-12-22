5c9b88984825a632c676aaf9cfd725e77fc1df08
Dayton Nolan <dnolan@gmail.com>
2012-12-22 01:31:24 -0600

    Refactor to update PATH reliably with current gemset's bin directory

--- auto_gemsets.sh
+++ auto_gemsets.sh
@@ -1,67 +1,123 @@
-function create_gemset_if_missing() {
-  if [ ! -d "${GEMSET_ROOT}/${GEMSET}" ]; then
-    mkdir -p "${GEMSET_ROOT}/${GEMSET}" && echo "${GEMSET} created. run gem install bundler"
+set_default_gemset() {
+  if [ -n "${DEFAULT_GEMSET}" ]; then
+    if [ ! "$GEM_PATH" == "${DEFAULT_GEMSET}" ]; then
+      DEFAULT=$( basename $DEFAULT_GEMSET )
+    fi
+
+    if [ -n "$GEMSET" ] && [ ! "$GEMSET" == "$DEFAULT" ]; then
+      remove_path "${GEMSET_ROOT}/${GEMSET}/bin"
+    fi
+
+    DEFAULT_BIN_PATH="${DEFAULT_GEMSET}/bin"
+
+    export GEMSET="${DEFAULT}"
+    export GEM_HOME="$DEFAULT_GEMSET"
+    export GEMFILE="$DEFAULT_GEMSET"
+    export GEM_ROOT="$GEM_HOME"
+    export GEM_PATH="$GEM_HOME"
+
+    add_path "$DEFAULT_BIN_PATH"
+    using_gemset_via "*default"
   fi
 }
 
-function auto_gemsets() {
+using_gemset_via() {
+  echo "Now using $GEMSET gemset via $1"
+}
+
+remove_path() {
+  NEW_PATH=""
+  IFS=':' read -a PATHS <<< "$PATH"
+  for p in "${PATHS[@]}"
+  do
+    if [ ! "$p" == "$1" ]; then
+      if [ -z "$NEW_PATH" ]; then
+        NEW_PATH="$p"
+      else
+        NEW_PATH="$NEW_PATH:$p"
+      fi
+    fi
+  done
+  export PATH="$NEW_PATH"
+}
+
+add_path () {
+  if ! echo $PATH | egrep -q "(^|:)$1($|:)" ; then
+     if [ "$2" = "after" ] ; then
+        PATH=$PATH:$1
+     else
+        PATH=$1:$PATH
+     fi
+
+     export PATH
+  fi
+}
+
+auto_gemsets() {
   local dir="$PWD"
-  local version_file
 
   until [[ -z "$dir" ]]; do
-    gemfile="$dir/Gemfile"
-    IFS='/' read -a gemfile_path_parts <<< "$gemfile"
-    gemset="${gemfile_path_parts[${#gemfile_path_parts[@]}-2]}"
+    GEMFILE="$dir/Gemfile"
 
-    if   [[ "${GEM_PATH//:$DEFAULT_GEMSET}" == "${GEMSET_ROOT}/$gemset" ]]; then return
-    elif [[ -f "$gemfile" ]]; then
-      export GEM_HOME="${GEMSET_ROOT}/$gemset"
-      export GEM_ROOT="${GEMSET_ROOT}/$gemset" # chruby specific
-      export GEM_PATH="${GEM_HOME}:${DEFAULT_GEMSET}"
-      echo "$PATH" | grep -q "${GEMSET_ROOT}/${gemset}/bin" || export PATH="$PATH:${GEMSET_ROOT}/${gemset}/bin"
-      export GEMSET="${gemset}"
-      export GEMFILE="${gemfile}"
-      create_gemset_if_missing && list_gemset
-      break
-    elif [[ ! -f "$gemfile" ]]; then
-      set_default_gemset
+    if [ -f "$GEMFILE" ]; then
+      get_parent_dirname
+      if [ "$GEMSET" == "$PARENT_DIR" ]; then
+        break
+      else
+        set_gemset "$PARENT_DIR"
+        break
+      fi
+    else
+      if [ ! "$GEMSET" == "$DEFAULT" ]; then
+        set_default_gemset
+      fi
     fi
 
     dir="${dir%/*}"
   done
 }
 
-function set_default_gemset() {
-  if [ ! -z "${DEFAULT_GEMSET}" ]; then
-    if [ ! "$GEM_PATH" == "${DEFAULT_GEMSET}" ]; then
-      export GEM_HOME="$DEFAULT_GEMSET"
-      export GEM_ROOT="$DEFAULT_GEMSET"
-      export GEM_PATH="$DEFAULT_GEMSET"
-      export GEMSET="daytonn"
-      export GEMFILE="*default"
+get_parent_dirname() {
+  IFS='/' read -a gemfile_path_parts <<< "$GEMFILE"
+  PARENT_DIR="${gemfile_path_parts[${#gemfile_path_parts[@]}-2]}"
+}
 
-      echo "$PATH" | grep -q "${DEFAULT_GEMSET}/bin" || export PATH="$PATH:${DEFAULT_GEMSET}/bin"
+set_gemset() {
+  NEW_GEMSET="$1"
+  NEW_GEMSET_PATH="${GEMSET_ROOT}/${1}"
+  NEW_GEMSET_BIN_PATH="${NEW_GEMSET_PATH}/bin"
+  remove_path "$GEMSET_BIN_PATH"
+  create_gemset_if_missing "$NEW_GEMSET"
+  export GEMSET="$NEW_GEMSET"
+  export GEM_HOME="$NEW_GEMSET_PATH"
+  export GEM_ROOT="$NEW_GEMSET_PATH"
+  export GEM_PATH="$NEW_GEMSET_PATH:$DEFAULT_GEMSET"
 
-      if [ -z "$GEMSET_PRELOAD" ]; then
-        list_gemset
-      fi
-    fi
+  add_path "$DEFAULT_BIN_PATH"
+  add_path "$NEW_GEMSET_BIN_PATH"
+
+  using_gemset_via "$GEMFILE"
+}
+
+create_gemset_if_missing() {
+  if [ ! -d "${GEMSET_ROOT}/${1}" ]; then
+    mkdir -p "${GEMSET_ROOT}/${1}" && echo "${1} created. run gem install bundler"
   fi
 }
 
-function list_gemset() {
-  echo "Now using ${GEMSET} gemset via ${GEMFILE}"
-}
-
+# Create a GEMSET_ROOT if none is set
 if [ ! -n "$GEMSET_ROOT" ]; then
   export GEMSET_ROOT="${HOME}/.gemsets"
 fi
 
+# If ZSH add precommand
 if [[ -n "$ZSH_VERSION" ]]; then
   precmd_functions+=("auto_gemsets")
 else
-  if [[ -n "$PROMPT_COMMAND" ]]; then
-    if [[ ! "$PROMPT_COMMAND" == *auto_gemsets* ]]; then
+  # If there's already a prompt command,
+  if [ -n "$PROMPT_COMMAND" ]; then
+    # check if it's already in the PROMPT
+    if [ ! "$PROMPT_COMMAND" == *auto_gemsets* ]; then
       PROMPT_COMMAND="$PROMPT_COMMAND; auto_gemsets"
     fi
   else
@@ -69,11 +125,5 @@ else
   fi
 fi
 
-function init() {
-  # Set default on load
-  GEMSET_PRELOAD="true"
-  set_default_gemset
-  unset GEMSET_PRELOAD
-}
-
-init
+# Set default when sourced
+set_default_gemset
